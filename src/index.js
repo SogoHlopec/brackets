1e78a703f4dc30813e7e2711dc40a1d21d9537dc
SogoHlopec <sogo246@gmail.com>
2022-08-07 02:36:12 +0300

    feat:add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,27 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+  let stack = [];
+  if (str.length % 2) {
+    return false
+  }
+  let openBr = bracketsConfig.map(el => el[0]);
+  console.log(openBr);
+  let closeBr = bracketsConfig.map(el => el[1]);
+  console.log(closeBr);
+  for (let i = 0; i < str.length; i++) {
+    if (openBr.includes(str[i])) {
+      stack.push(str[i])
+    }
+    if (closeBr.indexOf(str[i]) === openBr.indexOf(stack[stack.length - 1])) {
+      stack.pop(str[i])
+    }
+    if (openBr.includes(str[i]) && closeBr.includes(str[i])) {
+      if (stack[stack.length - 1] === str[i]) {
+        stack.pop();
+      }
+      else {
+        stack.push(str[i]);
+      }
+  }
+}
+  return stack.length === 0;
 }
